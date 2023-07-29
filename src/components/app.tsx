49cf61813057a342082e77b25a397e4b034e0c25
ms606 <muffi606@gmail.com>
2023-07-29 13:09:44 +0500

    UI updates according to excel

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,8 +1,13 @@
-import React, { FunctionComponent, useState} from 'react';
-import styled from 'styled-components';
-import { ZakekeEnvironment, ZakekeViewer, ZakekeProvider } from 'zakeke-configurator-react';
-import Selector from './selector';
-import { DialogsRenderer } from './dialog/Dialogs';
+import React, { FunctionComponent, useState, useRef, useEffect } from "react";
+import styled from "styled-components";
+import {
+  ZakekeEnvironment,
+  ZakekeViewer,
+  ZakekeProvider,
+} from "zakeke-configurator-react";
+import Selector from "./selector";
+import { DialogsRenderer } from "./dialog/Dialogs";
+import useStore from "../Store";
 
 // const Layout = styled.div`
 //     display: grid;
@@ -14,43 +19,128 @@ import { DialogsRenderer } from './dialog/Dialogs';
 // `
 
 const Layout = styled.div`
-    display: flex; 
-    flex  
-    height: 50%;
-    width: 50%
-    padding: 40px;
-    flex-direction: column;
-`
+  display: flex; 
+  flex  
+  height: 50%;
+  width: 50%
+  padding: 40px;
+  flex-direction: column;
+`;
+
+const MobileContainer = styled.div`
+  display: grid;
+  grid-template-rows: 1fr auto auto;
+  height: 100%;
+  width: 100%;
+  overflow: hidden;
+`;
 
 const zakekeEnvironment = new ZakekeEnvironment();
 
 const App: FunctionComponent<{}> = () => {
+  const {
+    isLoading,
+    setPriceFormatter,
+    setSelectedAttributeId,
+    setSelectedGroupId,
+    setSelectedStepId,
+    isMobile,
+    selectedGroupId,
+    setIsMobile,
+  } = useStore();
+
+  const [resize, setResize] = useState(false);
+  const resizeRef = useRef(false);
+  resizeRef.current = resize;
 
   // Update tray preview open button, to update width height for ThreeDRendered
-  const [selectedTrayPreviewOpenButton3D, selectTrayPreviewOpenButton3D] = useState< boolean | null>(false);
+  const [selectedTrayPreviewOpenButton3D, selectTrayPreviewOpenButton3D] =
+    useState<boolean | null>(false);
 
-  const trayPreviewOpenButton3DFunc = (selectedTrayPreviewOpenButton3D:any) => {
+  const trayPreviewOpenButton3DFunc = (
+    selectedTrayPreviewOpenButton3D: any
+  ) => {
     // console.log(selectedTrayPreviewOpenButton3D,'han bhae');
     selectTrayPreviewOpenButton3D(selectedTrayPreviewOpenButton3D);
   };
 
-    return <ZakekeProvider environment={zakekeEnvironment}> 
-     <div id='modal-container' className='css-1q5ttm8'>
-      <div className='css-1ecy5z8'>
-       <Layout>
-            <div style={{ display: 'grid', alignItems: 'center', justifyContent: 'center',gridArea: '1 / 2 / 12 / 1', backgroundColor: 'rgb(249 246 248)'}}>
-            <div className='ThreeDRenderer' style= {selectedTrayPreviewOpenButton3D ? {width:'70vw', height:'70vh'} : {width:'80vw', height:'80vh'}} 
-            // style={{width:'70vw', height:'70vh'}}
-            >
-                <ZakekeViewer />
-            </div>             
-            </div>
-            <Selector trayPreviewOpenButton3DFunc={trayPreviewOpenButton3DFunc}/>   
-        </Layout>
-        <DialogsRenderer />
-      </div>         
-     </div>
-     </ZakekeProvider>;
-}
+  // Page resize
+  useEffect(() => {
+    const resizeFunction = () => {
+      setResize(!resizeRef.current);
+    };
 
-export default App; 
+    window.addEventListener("resize", resizeFunction);
+    return () => window.removeEventListener("resize", resizeFunction);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return (
+    <ZakekeProvider environment={zakekeEnvironment}>
+      <div id="modal-container" className="css-1q5ttm8">
+        <div className="css-1ecy5z8">
+          {isMobile && (
+            <Layout>
+              <div
+                style={{
+                  display: "grid",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  backgroundColor: "rgb(249 246 248)",
+                }}
+              >
+                <div
+                  className="ThreeDRenderer"
+                  style={
+                    selectedTrayPreviewOpenButton3D
+                      ? { width: "20vw", height: "20vh" }
+                      : { 
+                        //width: "56vw", height: "55vh",
+                        aspectRatio: "1 / 1",
+                       width: "87%",  position: "absolute", top: "28px", bottom: "0", left: "11%" }
+                  }
+                >
+                  <ZakekeViewer />
+                </div>
+              </div>
+              <Selector
+                trayPreviewOpenButton3DFunc={trayPreviewOpenButton3DFunc}
+              />
+            </Layout>
+          )}
+
+          {!isMobile && (
+            <Layout>
+              <div
+                style={{
+                  display: "grid",
+                  alignItems: "center",
+                  justifyContent: "center",
+                  gridArea: "1 / 2 / 12 / 1",
+                  backgroundColor: "rgb(249 246 248)",
+                }}
+              >
+                <div
+                  className="ThreeDRenderer"
+                  style={
+                    selectedTrayPreviewOpenButton3D
+                      ? { width: "70vw", height: "70vh" }
+                      : { width: "80vw", height: "80vh" }
+                  }
+                >
+                  <ZakekeViewer />
+                </div>
+              </div>
+              <Selector
+                trayPreviewOpenButton3DFunc={trayPreviewOpenButton3DFunc}
+              />
+            </Layout>
+          )}
+          <DialogsRenderer />
+        </div>
+      </div>
+    </ZakekeProvider>
+  );
+};
+
+export default App;
