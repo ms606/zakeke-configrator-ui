32be200a14427a8e62b2005c2b6268443a443f82
muffi606 <muffi606@gmail.com>
2023-08-25 17:02:45 +0500

    product view

--- src/components/selector.tsx
+++ src/components/selector.tsx
@@ -420,10 +420,10 @@ const Selector: FunctionComponent<TrayPreviewOpenButton3DProps> = ({
               className="LayoutStyled__GroupItem-sc-1nws045-2"
               style={{
                 position: "absolute",
-                top: "-17%",
-                left: "10em",
+                top: "-42%",
+                left: "11.2em",
                 cursor: "pointer",
-                width: "90px",
+                width: "70px",
                 height: "51px"
               }}
             >
