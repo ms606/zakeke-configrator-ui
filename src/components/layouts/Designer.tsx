84ea63f7cf7f687c835fc763f7b4e0976c50d81c
ms606 <muffi606@gmail.com>
2023-09-13 13:43:35 +0500

    Button color

--- src/components/layouts/Designer.tsx
+++ src/components/layouts/Designer.tsx
@@ -835,7 +835,7 @@ const Designer: FC<{
 							<SearchMinusSolid />
 						</ZoomIconOut> */}
           {/* </IconsAndDesignerContainer> */}
-         <div style={{position: "relative", top: "32px"}}>
+         <div style={{position: "relative", top: "30px"}}>
          <Button isFullWidth primary onClick={() => setMoveElements(false)}>
             <span>{"OK"} </span>
           </Button>
